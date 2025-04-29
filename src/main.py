b0d7c8da8f317bc0c676059163226fe7da8ff8b7
LuisHenrique2000 <134005875+LuisHenrique2000@users.noreply.github.com>
2025-04-29 19:35:33 -0300

    Delete src/main.py

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-def create_app():
-    app = Flask(__name__)
-    app.config.from_mapping(SECRET_KEY="trocar-por-chave-segura")
-    @app.route("/")
-    def home():
-        return "Receitas Sem Fogo - Portal Online"
-    return app
